7408a6c40902b3026265a28b707a32fc367af278
sayra17 <agussayra03@gmail.com>
2022-02-03 19:23:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+04-Javascript-TUGAS-12-JS
---
+++ tugas12.js
@@ -0,0 +1,9 @@
+function buah_buahan() {
+    var buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+    console.log(buah)
+    buah.pop()
+    console.log(buah)
+    buah.shift()
+    return buah
+}
+console.log(buah_buahan());
